54cd93eff8781cd12f9bf3a55d3038f3d64d4218
Oscar Heimdahl <o.heimdahl@gmail.com>
2020-04-06 09:46:53 +0200

    added hover and click to project images.

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './project.css';
 
-export const Project = props => {
+export const Project = (props) => {
   return (
     <div className="project">
       <h1>{props.title}</h1>
-      <img src={props.image} alt="bild"></img>
+      <a href={props.link} target="_blank" rel="noopener noreferrer">
+        <div className="image-container">
+          <img src={props.image} alt="bild"></img>
+        </div>
+      </a>
       <p>{props.text}</p>
     </div>
   );
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -16,11 +16,11 @@ export class Projects extends Component {
     this.state = {};
     this.projects = React.createRef();
     this.state = {
-      toggled: false
+      toggled: false,
     };
   }
 
-  close = event => {
+  close = (event) => {
     this.props.close(event, this.projects);
   };
 
@@ -29,13 +29,13 @@ export class Projects extends Component {
       <div
         ref={this.projects}
         className="projects-background"
-        onClick={event => this.close(event)}
-        onScroll={event => {
+        onClick={(event) => this.close(event)}
+        onScroll={(event) => {
           if (event.target.scrollTop < -50) this.props.close(event);
         }}
         style={{
           pointerEvents: this.props.toggled ? 'initial' : 'none',
-          overflow: this.props.toggled ? 'auto' : 'hidden'
+          overflow: this.props.toggled ? 'auto' : 'hidden',
         }}
       >
         <div
@@ -46,85 +46,72 @@ export class Projects extends Component {
             className="close-button"
             src={close}
             alt="Stäng"
-            onClick={event => this.close(event)}
+            onClick={(event) => this.close(event)}
           />
           <Project
             title="Typer"
             image={typer}
+            link="https://typer-14eb6.firebaseapp.com/"
             text={[
               'En sida där man kan tävla mot andra i att skriva snabbt. Sidan är skriven i React och använder Firebase som backend för att lagra texter och cloud functions för att kommunicera mellan spelarna.',
-              <br />,
-              <br />,
-              'Sidan hittar du ',
-              <a href="https://typer-14eb6.firebaseapp.com/">här!</a>
             ]}
           ></Project>
           <Project
             title="Chatter-O"
+            link="https://chatter-o.herokuapp.com/"
             image={chatter}
             text={[
               'En hemsida som håller en öppen chat mellan alla som ansluter.',
               <br />,
               <br />,
               'Frontend byggd i React och en backend i NodeJS, med MongoDB som databas. Både klienten och servern är uppladdade på heroku.',
-              <br />,
-              <br />,
-              <a href="https://chatter-o.herokuapp.com/">Chatta här!</a>
             ]}
           ></Project>
           <Project
             title="Internship på Alvira"
             image={alvira}
+            link="https://alvira.se"
             text={[
-              'På',
-              <a href="https://alvira.se"> Alvira </a>,
-              'så utvecklade jag UI:t av deras mobilapp. Flutter användes för att kunna bygga till både Android och iOS. Utöver att bygga mobila applikationer så fick jag också erfarenhet i att arbeta i ett litet team och tillsammans lösa problem.'
+              'På Alvira så utvecklade jag UI:t av deras mobilapp. Flutter användes för att kunna bygga till både Android och iOS. Utöver att bygga mobila applikationer så fick jag också erfarenhet i att arbeta i ett litet team och tillsammans lösa problem.',
             ]}
           ></Project>
           <Project
             title="Konsultuppdrag åt Arboreal"
             image={arboreal}
+            link="https://www.arboreal.se/"
             text={[
-              'I ett skolprojekt hjälpte vi startupen',
-              <a href="https://www.arboreal.se/"> Arboreal </a>,
-              'med ett designförslag på hur deras app kunde förbättras både funktions- och designmässigt.',
+              'I ett skolprojekt hjälpte vi startupen Arboreal med ett designförslag på hur deras app kunde förbättras både funktions- och designmässigt.',
               <br />,
               <br />,
-              'Arbetetet gick bl.a. ut på att göra en omvärldsanalys där vi tog reda på hur målgruppen skulle använda appen. Vår hi-fi prototyp skapad i Adobe XD.'
+              'Arbetetet gick bl.a. ut på att göra en omvärldsanalys där vi tog reda på hur målgruppen skulle använda appen. Vår hi-fi prototyp skapad i Adobe XD.',
             ]}
           ></Project>
           <Project
             title="25 Minutes"
+            link="https://github.com/oscarheimdahl/break-timer/releases/tag/v1.0"
             image={twentyfive}
             text={[
-              'Ett native program till windows och macOS som påminner en om att ofta ta raster.'
+              'Ett native program till windows och macOS som påminner en om att ofta ta raster.',
             ]}
           ></Project>
           <Project
             title="Gradient Picker"
             image={gradient}
+            link="https://oscarheimdahl.github.io/gradient-picker/"
             text={[
-              ' Jag byggde ett smidigt verktyg i React för att enkelt kunna se på olika gradient i CSS. Verktyget tillåter en att exportera gradienterna som CSS eller som en PNG.',
-              <br />,
-              'Ni kan kika på den',
-              <a href="https://oscarheimdahl.github.io/gradient-picker/">
-                {' '}
-                här!
-              </a>
+              ' Jag byggde ett smidigt verktyg i React för att enkelt kunna se på olika gradienter i CSS. Verktyget tillåter en att exportera gradienterna som CSS eller som en PNG.',
             ]}
           ></Project>
           <Project
             title="Tetris"
             image={tetris}
+            link="https://oscarheimdahl.github.io/tetris/"
             text={[
               'Tetris!',
               <br />,
               'Byggt i JavaScript med biblioteket',
               <a href="https://p5js.org/"> p5.js </a>,
               'för att rita på canvas.',
-              <br />,
-              'Spela',
-              <a href="https://oscarheimdahl.github.io/tetris/"> här!</a>
             ]}
           ></Project>
         </div>
